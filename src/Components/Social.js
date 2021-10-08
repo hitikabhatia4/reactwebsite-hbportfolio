a5a6ac79e4fc08f4158722b4ef1a523a5518a642
Hitika <bhatiahitika4@gmail.com>
2021-10-08 18:27:45 +0530

    init

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,43 +1,48 @@
 import Icon from "@chakra-ui/icon";
-import { HStack, Stack, Text } from "@chakra-ui/layout";
+import { Flex, HStack, Stack, Text , VStack} from "@chakra-ui/layout";
 import { useMediaQuery } from "@chakra-ui/media-query";
 import React  from "react";
 import {FaHtml5,FaBootstrap,FaCss3,FaFigma,FaJava,FaPython,FaReact,FaJavascript} from 'react-icons/fa';
 function Social() {
-   const [isNotSmallerScreen] =useMediaQuery("(min-width:200px)");
+   const [isNotSmallerScreen] =useMediaQuery("(min-width:600px)");
+   const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)")
     return (
        <>
-      <Text fontWeight="semibold" fontSize="4xl" as="u" >Skills</Text>
-      <HStack spacing={5} marginTop="32"  mt={isNotSmallerScreen ? "0" : 32}>
-         <Stack mt={isNotSmallerScreen ? "0" : 32} >
-         <Icon as={FaHtml5}  h="20" w="20" ></Icon >
-         <Text textAlign="center" fontSize="20">Html</Text>
-         </Stack>
-         <Stack>
-         <Icon as={FaBootstrap} h="20" w="20"></Icon>
-         <Text textAlign="center" fontSize="20">Bootstrap</Text>
-         </Stack>
-         <Stack>
-         <Icon as={FaCss3} h="20" w="20"></Icon>
-         <Text  textAlign="center" fontSize="20">Css</Text>
-         </Stack>
-         <Stack>
-         <Icon as={FaFigma} h="20" w="20"></Icon>
-         <Text  textAlign="center" fontSize="20">Figma</Text>
-         </Stack>
-         <Stack>
-         <Icon as={FaJava} h="20" w="20"></Icon>
-         <Text  textAlign="center" fontSize="20">Java</Text>
-         </Stack>
-         <Stack>
-         <Icon as={FaPython} h="20" w="20"></Icon>
-         <Text  textAlign="center" fontSize="20">Python</Text>
-         </Stack>
-         <Stack>
-         <Icon as={FaReact} h="20" w="20"></Icon>
-         <Text  textAlign="center" fontSize="20">React</Text>
-         </Stack>
-      </HStack>
+      <Text fontWeight="semibold" fontSize="4xl" as="u"  >Skills</Text>
+      <Flex padding={10} direction={isNotSmallerScreen ? "row" : "column"} >
+      
+      <Stack pr="10" ml="10">
+      <Icon as={FaHtml5}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">Html </Text>
+      </Stack>
+      <Stack pr="10" ml="10">
+      <Icon as={FaBootstrap}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">Bootstrap</Text>
+      </Stack>
+      <Stack pr="10" ml="10">
+      <Icon as={FaCss3}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">Css</Text>
+      </Stack>
+      <Stack pr="10" ml="10">
+      <Icon as={FaFigma}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">Figma</Text>
+      </Stack>
+      <Stack pr="10" ml="10">
+      <Icon as={FaJava}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">Java</Text>
+      </Stack>
+      <Stack pr="10" ml="10">
+      <Icon as={FaPython}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">Python</Text>
+      </Stack>
+      <Stack pr="10" ml="10">
+      <Icon as={FaReact}  h="20" w="20"></Icon>
+      <Text textAlign="center" fontSize="20">React</Text>
+      </Stack>
+      
+      </Flex>
+      
+      
      </>
     );
   }
